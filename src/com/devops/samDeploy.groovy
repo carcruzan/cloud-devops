2d06bb2c30f1bd3efdb5ce0637c8196737e71870
carcruzan <36182856+carcruzan@users.noreply.github.com>
2023-03-24 15:39:07 -0500

    Update samDeploy.groovy

--- src/com/devops/samDeploy.groovy
+++ src/com/devops/samDeploy.groovy
@@ -12,7 +12,6 @@ def samDeploy() {
         
         stack_name=$(cat config.json | jq -r '.[] | select(.ParameterKey=="stackName") | .ParameterValue ')
 
-        sam build
         sam deploy -t final_template.yaml --stack-name=$stack_name --region=$region --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS --tags Key=Name,Value=$stack_name
 
     '''
