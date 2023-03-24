f35fc2d8c44b8013bf1099d6620a84e2cc536ec7
Carlos E. Cruz  Rodriguez <carlosrodriguez@cbit-online.com>
2023-03-24 11:23:43 -0500

    Refactorizacion shared library

--- src/com/devops/samDeploy.groovy
+++ src/com/devops/samDeploy.groovy
@@ -9,8 +9,8 @@ def samDeploy() {
     sh '''
         chmod a+x template.yaml
 
-        SAM_PARAMETERS=$( cat config.json | jq -r '.[] | "\(.ParameterKey)=\(.ParameterValue)"' )
-
+        // SAM_PARAMETERS=$( cat config.json | jq -r '.[] | "\(.ParameterKey)=\(.ParameterValue)"' )
+        SAM_PARAMETERS=$( cat config.json)
         sam deploy -t template.yaml --stack-name=${config[].ParameterKey == 'stack-name'} --region=us-east-1 --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS
     '''
 }
