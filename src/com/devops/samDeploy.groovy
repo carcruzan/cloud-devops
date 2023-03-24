f6ae4eb71cfaa66645777eade3e6221b45294a6c
carcruzan <36182856+carcruzan@users.noreply.github.com>
2023-03-24 15:09:11 -0500

    Update samDeploy.groovy

--- src/com/devops/samDeploy.groovy
+++ src/com/devops/samDeploy.groovy
@@ -11,8 +11,13 @@ def samDeploy() {
         region=$(cat config.json | jq -r '.[] | select(.ParameterKey=="region")| .ParameterValue ')
         
         stack_name=$(cat config.json | jq -r '.[] | select(.ParameterKey=="stackName") | .ParameterValue ')
-        
-        sam deploy -t template.yaml --stack-name=$stack_name --region=$region --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS --tags Key=Name,Value=$stack_name
+
+        echo Building...
+        sam build
+        echo Packaging...
+        sam package --s3-bucket=iac-training-ecs -t template.yaml --output-template-file=final_template.yaml
+        echo Deplopying...
+        sam deploy -t final_template.yaml --stack-name=$stack_name --region=$region --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS --tags Key=Name,Value=$stack_name
 
     '''
 }
