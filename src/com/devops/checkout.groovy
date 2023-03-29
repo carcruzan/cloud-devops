09cf0e6608c484e1bae436527916d5ec90225c1a
Carlos E. Cruz  Rodriguez <carlosrodriguez@cbit-online.com>
2023-03-29 11:39:11 -0500

    Refactor samDeploy

--- src/com/devops/checkout.groovy
+++ src/com/devops/checkout.groovy
@@ -1,5 +1,12 @@
 package com.devops
 
 def checkOut(repo) {
-  git branch: 'main', url: "${repo}"
+  // git branch: 'main', url: "${repo}"
+  checkout([
+      $class: 'GitSCM',
+      branches: 'develop',
+      extensions: scm.extensions + [[$class: 'LocalBranch'], [$class: 'WipeWorkspace']],
+      userRemoteConfigs: [[url: '${repo}']],
+      doGenerateSubmoduleConfigurations: false
+  ])
 }
