0c95cec72ff1c86aab22246472ee371d691317cf
Carlos E. Cruz  Rodriguez <carlosrodriguez@cbit-online.com>
2023-03-29 12:43:22 -0500

    Refactor samDeploy

--- src/com/devops/samDeploy.groovy
+++ src/com/devops/samDeploy.groovy
@@ -5,24 +5,23 @@ def samDeploy() {
     writeFile(file: 'template.yaml', text: template)
 
     sh "sed -i 's/ENVIRONMENT_ID/${ENVIRONMENT_ID}/gI' config.json"
-    sh "cat config.json"
-    // sh '''
+    sh '''
 
-    //     PathCodeLambda=$(cat config.json | jq -r '.properties[] | .PathCodeLambda')
-    //     ZipName=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="ZipName")| .ParameterValue ')
-    //     Bucket=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="Bucket")| .ParameterValue ')
-    //     SAM_PARAMETERS=$( cat config.json | jq -r '.deploy[] | "\\(.ParameterKey)=\\(.ParameterValue)"')
-    //     region=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="region")| .ParameterValue ')
-    //     stack_name=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="stackName") | .ParameterValue ')
+        PathCodeLambda=$(cat config.json | jq -r '.properties[] | .PathCodeLambda')
+        ZipName=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="ZipName")| .ParameterValue ')
+        Bucket=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="Bucket")| .ParameterValue ')
+        SAM_PARAMETERS=$( cat config.json | jq -r '.deploy[] | "\\(.ParameterKey)=\\(.ParameterValue)"')
+        region=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="region")| .ParameterValue ')
+        stack_name=$(cat config.json | jq -r '.deploy[] | select(.ParameterKey=="stackName") | .ParameterValue ')
 
-    //     cd $PathCodeLambda
+        cd $PathCodeLambda
 
-    //     zip -r $ZipName *
-    //     aws s3 cp $ZipName s3://$Bucket
+        zip -r $ZipName *
+        aws s3 cp $ZipName s3://$Bucket
 
-    //     cd ..
+        cd ..
 
-    //     sam deploy -t template.yaml --stack-name=$stack_name --region=${env.DEFAULT_REGION} --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS
+        sam deploy -t template.yaml --stack-name=$stack_name --region=${env.DEFAULT_REGION} --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides $SAM_PARAMETERS
 
-    // '''
+    '''
 }
